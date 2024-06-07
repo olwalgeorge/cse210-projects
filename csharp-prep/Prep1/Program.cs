3659df78e28b813f554c5d363c6f4801708d5d8e
George Olwal <olwalgeorge@live.com>
2024-06-07 13:18:55 +0300

    Prep1 Assignment

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+        string capitalizedFirstName = char.ToUpper(firstName[0]) + firstName.Substring(1);
+
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+        string capitalizedLastName = char.ToUpper(lastName[0]) + lastName.Substring(1);
+        
+        Console.Write($"Your name is {capitalizedLastName}, {capitalizedFirstName} {capitalizedLastName}.");
+        
+
+
     }
 }
