458dd4f1e9357eeb6153a1523fdc2b7fbb8863e9
George Olwal <olwalgeorge@live.com>
2024-06-07 23:10:21 +0300

    Prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random random = new Random();
+        int magicNumber = random.Next(1, 101);
+        
+        int guess = 0;
+        int attempts = 0;
+        while(guess != magicNumber)
+            {
+                attempts++;
+                {
+                    Console.Write("What is your guess? ");
+                    guess = int.Parse(Console.ReadLine());                    
+                }
+                if(guess > magicNumber)
+                    {
+                        Console.WriteLine("Lower");
+                    }  
+                else if(guess < magicNumber)
+                    {
+                        Console.WriteLine("Higher");
+                    }  
+                
+            }
+        Console.WriteLine($"You guessed it! It took you {attempts} guesses");
+        Console.Write("Do you want to continue with the game? ");
+        string answer = Console.ReadLine();
+        if(answer == "y")
+            {   
+                Main(args);
+
+            }  
+        else
+            {
+                Console.WriteLine("Goodbye");
+            }
     }
+
+
 }
